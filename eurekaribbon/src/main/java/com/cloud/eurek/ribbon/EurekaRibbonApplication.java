6876efd2534eb6655e6b067fc14a9e343ef9f366
wubo <wubo@ppdai.com>
2016-10-09 13:27:14 +0800

    使用Ribbon实现客户端负载均衡的消费者

---
+++ eurekaribbon/src/main/java/com/cloud/eurek/ribbon/EurekaRibbonApplication.java
@@ -0,0 +1,12 @@
+package com.cloud.eurek.ribbon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurekaRibbonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurekaRibbonApplication.class, args);
+	}
+}
