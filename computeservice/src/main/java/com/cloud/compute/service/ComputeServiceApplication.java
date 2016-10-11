58c2dd0ba4fa21514fcd763cc7abb4a4e0cf61f9
wubo <wubo@ppdai.com>
2016-10-11 09:13:18 +0800

    修复no bean named  DiscoveryClient

--- computeservice/src/main/java/com/cloud/compute/service/ComputeServiceApplication.java
+++ computeservice/src/main/java/com/cloud/compute/service/ComputeServiceApplication.java
@@ -2,8 +2,9 @@ package com.cloud.compute.service;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
-//@EnableDiscoveryClient
+@EnableDiscoveryClient
 @SpringBootApplication
 public class ComputeServiceApplication {
 
