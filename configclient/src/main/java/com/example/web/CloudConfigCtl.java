0efc4fcf96d5099e325821e943009d47a27f8a95
wubo <wubo@ppdai.com>
2016-10-20 16:58:48 +0800

    新增配置文件

---
+++ configclient/src/main/java/com/example/web/CloudConfigCtl.java
@@ -0,0 +1,21 @@
+package com.example.web;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by wubo on 2016/10/20.
+ */
+@RestController
+public class CloudConfigCtl {
+
+    @Value("{name}")
+    private String name;
+
+    @RequestMapping(value = "/check", method = RequestMethod.GET)
+    public String check(){
+        return name;
+    }
+}
