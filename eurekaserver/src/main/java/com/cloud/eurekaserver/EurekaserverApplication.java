53f3692bebad8eff7996c94ed6436dc1c29b8cd8
wu05281 <37809131@qq.com>
2017-09-20 14:29:37 +0800

    spring cloud zuul+eureka 服务负载均衡

--- eurekaserver/src/main/java/com/cloud/eurekaserver/EurekaserverApplication.java
+++ eurekaserver/src/main/java/com/cloud/eurekaserver/EurekaserverApplication.java
@@ -9,7 +9,6 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 public class EurekaserverApplication {
 
 	public static void main(String[] args) {
-		System.out.println("this is a test");
 		SpringApplication.run(EurekaserverApplication.class, args);
 	}
 }
