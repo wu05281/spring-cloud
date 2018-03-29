e8b0bae8f7c73860d63249b8c0bc7e2396e05221
wu05281 <37809131@qq.com>
2018-03-29 15:06:39 +0800

    eureka feign调用以及 cloud与boot版本问题修正

--- eurekafeign/src/main/java/com/wu/cloud/feign/EurekaFeignApplication.java
+++ eurekafeign/src/main/java/com/wu/cloud/feign/EurekaFeignApplication.java
@@ -1,9 +1,20 @@
 package com.wu.cloud.feign;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.feign.EnableFeignClients;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
 
 @SpringBootApplication
+@Configuration
+@ComponentScan
+@EnableAutoConfiguration
+@EnableEurekaClient
+@EnableFeignClients
 public class EurekaFeignApplication {
 
 	public static void main(String[] args) {
--- eurekafeign/src/main/java/com/wu/cloud/feign/web/ConsumerController.java
+++ eurekafeign/src/main/java/com/wu/cloud/feign/web/ConsumerController.java
@@ -1,23 +1,20 @@
-package com.cloud.eurek.ribbon.web;
+package com.wu.cloud.feign.web;
 
+import com.wu.cloud.feign.service.ComputeClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.client.RestTemplate;
 
-/**
- * Created by wubo on 2016/10/9.
- */
 @RestController
 public class ConsumerController {
 
     @Autowired
-    RestTemplate restTemplate;
+    ComputeClient computeClient;
 
     @RequestMapping(value = "/add", method = RequestMethod.GET)
-    public String add() {
-        return restTemplate.getForEntity("http://service-add/add?a=10&b=20", String.class).getBody();
+    public Integer add() {
+        return computeClient.add(10, 20);
     }
 
 }
