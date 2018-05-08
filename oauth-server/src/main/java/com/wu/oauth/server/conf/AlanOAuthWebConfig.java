c7fe978d769bcc1697f1f2d8fb83a8b9be31e3c2
wu05281 <37809131@qq.com>
2018-05-08 16:35:32 +0800

    定制授权认证中心自有的登录页面

--- oauth-server/src/main/java/com/wu/oauth/server/conf/AlanOAuthWebConfig.java
+++ oauth-server/src/main/java/com/wu/oauth/server/conf/AlanOAuthWebConfig.java
@@ -13,6 +13,7 @@ public class AlanOAuthWebConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         super.configure(http);
+        http.formLogin().loginPage("/toLogin").permitAll();
     }
 
     @Override
