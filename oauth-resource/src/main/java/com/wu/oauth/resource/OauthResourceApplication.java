522dcc7efcd5e2a82699f1af646e2b055348fdd1
wu05281 <37809131@qq.com>
2018-05-25 14:51:35 +0800

    授权认证的resource以及client

---
+++ oauth-resource/src/main/java/com/wu/oauth/resource/OauthResourceApplication.java
@@ -0,0 +1,12 @@
+package com.wu.oauth.resource;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OauthResourceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OauthResourceApplication.class, args);
+	}
+}
